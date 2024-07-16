80e26a42282e14dcf81081b608b3140deba377c4
dblackpearl <efe4dbest@gmail.com>
2024-07-16 17:18:25 -0400

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,40 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    
+    const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+    
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        
+        
+
+        if (totalExpenses > newBudget) {
+            alert('Exp greater');
+            setNewBudget(budget)
+            return;
+            
+        } 
+        
+       
+
+            setNewBudget(event.target.value);
+
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: newBudget
+            });
+    
+  
     }
+
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
