9f8fd280934ac6840ba51684cd96f94d70aab43e
dblackpearl <efe4dbest@gmail.com>
2024-07-18 20:10:32 -0400

    changes made to budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,25 +4,23 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget, expenses, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [currency, setCurrency] = useState('')
     
     const totalExpenses = expenses.reduce((total, item) => {
             return (total = total + item.cost);
         }, 0);
     
     const handleBudgetChange = (event) => {
-        
-        
 
         if (totalExpenses > newBudget) {
-            alert('Exp greater');
+            alert('You cannot reduce the budget value lower than the spending');
             setNewBudget(budget)
             return;
             
         } 
         
-       
-
-            setNewBudget(event.target.value);
+            
+        setNewBudget(event.target.value);
 
             dispatch({
                 type: 'SET_BUDGET',
@@ -34,7 +32,16 @@ const Budget = () => {
 
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<div style={{ }}>
+    <label htmlFor='currency'>Currency</label>
+        <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setCurrency(event.target.value)}>
+            <option defaultValue value="Dollar" name="Dollar">$ Dollar</option>
+            <option value="Pound" name="Pound">£ Pound</option>
+            <option value="Euro" name="Euro">€ Euro</option>
+            <option value="Ruppee" name="Ruppee">₹ Ruppee</option>
+        </select>
+    </div>
+{/* <span>Budget: £{budget}</span> */}
 <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
 </div>
     );
